3895278799f351b71e471fde0572290afd05c634
Ofredy <orellanafredy96@gmail.com>
2024-11-17 16:40:49 -0600

    completed att monte sim

--- rover_sim/tracking_attitude_sim/rover_att_monte.py
+++ rover_sim/tracking_attitude_sim/rover_att_monte.py
@@ -9,39 +9,85 @@ simulation_hz = 100  # 10 Hz simulation frequency
 simulation_time = 5  # Run for 5 seconds
 NUM_MONTE_RUNS = 10  # Number of Monte Carlo runs
 
-process_noise_var = 0.00000000000000001
+process_noise_var = 0.01
+
+estimation_hz = 10   # Estimation frequency (Hz)
 
 
-def get_beta_p_t(beta_t, t, freq_modifier):
+def get_beta_p_t(beta_t, t, freq_modifier=1.0, angular_velocity_profile=None):
+    """
+    Computes the quaternion derivative and angular velocity vector.
+
+    Parameters:
+    - beta_t: Current quaternion state.
+    - t: Current time.
+    - freq_modifier: Frequency modifier for angular velocity.
+    - angular_velocity_profile: Callable for angular velocity as a function of time.
+
+    Returns:
+    - Quaternion derivative and angular velocity vector.
+    """
     b0, b1, b2, b3 = beta_t[0], beta_t[1], beta_t[2], beta_t[3]
     beta_matrix = np.array([[-b1, -b2, -b3],
                             [b0, -b3, b2],
                             [b3, b0, -b1],
                             [-b2, b1, b0]])
-    # Angular velocity with varying frequency
-    w_t = np.array([0.0, 0.0, 35.0]) * np.pi / 180
-    return 0.5 * np.dot(beta_matrix, w_t), w_t  # Returns quaternion derivative and angular velocity vector
 
-def integrator(func_x, x_0, t_0, t_f, dt=(1 / simulation_hz), norm_value=False):
+    # Angular velocity for yaw-only motion
+    if angular_velocity_profile:
+        w_t = angular_velocity_profile(t)
+    else:
+        w_t = np.array([0.0, 0.0, 35.0]) * np.pi / 180  # Default yaw angular velocity
+
+    w_t *= freq_modifier  # Scale by frequency modifier
+    return 0.5 * np.dot(beta_matrix, w_t), w_t
+
+def integrator(func_x, x_0, t_0, t_f, dt=(1 / simulation_hz), norm_value=False, **kwargs):
+    """
+    Numerical integrator to compute state evolution over time.
+    
+    Parameters:
+    - func_x: Function to compute the derivative.
+    - x_0: Initial state.
+    - t_0: Start time.
+    - t_f: End time.
+    - dt: Time step.
+    - norm_value: Whether to normalize the state after each step.
+    - kwargs: Additional arguments to pass to the derivative function.
+    
+    Returns:
+    - state_summary: List of states over time.
+    """
     state_summary = []
     x_n = x_0
     state_summary.append(x_n)
     t = t_0
     while t <= t_f:
-        f = func_x(x_n, t)
+        f = func_x(x_n, t, **kwargs)  # Pass additional parameters to func_x
         x_n = x_n + f * dt
         if norm_value:
             x_n = x_n / np.linalg.norm(x_n)
         state_summary.append(x_n)
         t += dt
     return state_summary
+    
+# Define yaw-only angular velocity profiles
+def angular_velocity_profile_1(t):
+    return np.array([0.0, 0.0, 35.0 * np.sin(0.1 * t)]) * np.pi / 180
+
+def angular_velocity_profile_2(t):
+    return np.array([0.0, 0.0, 30.0 * np.cos(0.05 * t)]) * np.pi / 180
+
+def angular_velocity_profile_3(t):
+    return np.array([0.0, 0.0, 40.0]) * np.pi / 180  # Constant yaw velocity
 
 def generate_attitude_changes(
         initial_quat=np.array([1.0, 0.0, 0.0, 0.0]),
         time_step=(1 / simulation_hz),
         num_steps=int(simulation_time / (1 / simulation_hz)),
-        monte_runs=NUM_MONTE_RUNS
-):
+        monte_runs=NUM_MONTE_RUNS,
+        acceleration_std=0.1 ):
+
     attitudes = np.zeros((monte_runs, num_steps, 4))
     accelerations = np.zeros((monte_runs, num_steps, 3))
     magnetic_fields = np.zeros((monte_runs, num_steps, 3))
@@ -50,11 +96,16 @@ def generate_attitude_changes(
     gravity_vector = np.array([0.0, 0.0, -1])
     magnetic_field_vector = np.array([1.0, 0.0, 0.0])
 
+    profiles = [angular_velocity_profile_1, angular_velocity_profile_2, angular_velocity_profile_3]
+
     for monte_idx in range(monte_runs):
+        # Assign yaw-only profile for each run
+        angular_velocity_profile = profiles[monte_idx % len(profiles)]
+
         freq_modifier = 0.1 + 0.05 * monte_idx
         t_f = (num_steps - 1) * time_step
         attitude_sequence = integrator(
-            lambda beta, t: get_beta_p_t(beta, t, freq_modifier)[0],
+            lambda beta, t: get_beta_p_t(beta, t, freq_modifier, angular_velocity_profile=angular_velocity_profile)[0],
             initial_quat,
             0,
             t_f,
@@ -65,22 +116,25 @@ def generate_attitude_changes(
 
         for step in range(num_steps):
             current_quat = attitudes[monte_idx, step]
-            _, angular_velocity = get_beta_p_t(current_quat, step * time_step, freq_modifier)
+            _, angular_velocity = get_beta_p_t(current_quat, step * time_step, freq_modifier,
+                                               angular_velocity_profile=angular_velocity_profile)
             gyros[monte_idx, step] = angular_velocity
 
-            # Simulate accelerometer and magnetometer readings with noise
+            # Simulate accelerometer readings with gravity and noise in x, y directions
             rotation = Rotation.from_quat(current_quat[[1, 2, 3, 0]])  # Convert to [qx, qy, qz, qw] order for SciPy
             accel_body_frame = rotation.apply(gravity_vector)
-            accelerations[monte_idx, step] = accel_body_frame + np.random.normal(0, np.sqrt(process_noise_var))
 
+            # Add random noise to x, y directions
+            random_acceleration = np.array([
+                np.random.normal(0, acceleration_std),  # Random acceleration in x
+                np.random.normal(0, acceleration_std),  # Random acceleration in y
+                0  # No noise in z
+            ])
+            accelerations[monte_idx, step] = accel_body_frame + random_acceleration
+
+            # Simulate magnetometer readings
             mag_body_frame = rotation.apply(magnetic_field_vector)
             magnetic_fields[monte_idx, step] = mag_body_frame + np.random.normal(0, np.sqrt(process_noise_var))
-
-            # Estimate quaternion using TRIAD
-            estimated_quat = triad_method(accel_body_frame, mag_body_frame)
-            estimated_quat = normalize_quaternion(estimated_quat)
-
-            print(current_quat, estimated_quat)
 
     return {'att': attitudes, 'acc': accelerations, 'mag': magnetic_fields, 'gyro': gyros}
 
@@ -109,34 +163,33 @@ def visualize_quaternion_trajectories(attitudes, dt=(1 / simulation_hz)):
 
     plt.tight_layout(rect=[0, 0.03, 1, 0.95])
 
-def monte_att_estimate(monte_sum, dt=(1 / simulation_hz), accel_noise_std=1e-10, mag_noise_std=1e-10):
+def monte_att_estimate(monte_sum):
     """
-    Runs the TRIAD method for attitude estimation over multiple Monte Carlo runs 
-    using accelerometer and magnetometer data with added noise, with sign consistency check.
+    Runs the TRIAD method for attitude estimation at a lower frequency than the simulation.
     
     Parameters:
-    - monte_sum (dict): Contains arrays for true attitudes, accelerometer, magnetometer, and gyroscope data.
-    - dt (float): Time step.
-    - accel_noise_std (float): Standard deviation of noise to add to accelerometer data.
-    - mag_noise_std (float): Standard deviation of noise to add to magnetometer data.
+    - monte_sum: Contains arrays for true attitudes, accelerometer, magnetometer, and gyroscope data.
     
     Returns:
-    - estimated_states_all (np.ndarray): 3D array with shape (monte_runs, num_steps, 4) storing consistent estimated quaternions.
+    - estimated_states_all: 3D array with shape (monte_runs, num_estimation_steps, 4).
     """
+    estimation_step_interval = simulation_hz // estimation_hz
     monte_runs, num_steps = monte_sum['gyro'].shape[0], monte_sum['gyro'].shape[1]
-    estimated_states_all = np.zeros((monte_runs, num_steps, 4))
+    num_estimation_steps = num_steps // estimation_step_interval
+
+    estimated_states_all = np.zeros((monte_runs, num_estimation_steps, 4))
 
     for monte_idx in range(monte_runs):
         accelerations = monte_sum['acc'][monte_idx]
         magnetic_fields = monte_sum['mag'][monte_idx]
-        estimated_states = np.zeros((num_steps, 4))
+        estimated_states = np.zeros((num_estimation_steps, 4))
 
-        for step in range(num_steps):
-            acceleration = accelerations[step]
-            magnetic_field = magnetic_fields[step]
-            noisy_accel = acceleration + np.random.normal(0, accel_noise_std, 3)
-            noisy_mag = magnetic_field + np.random.normal(0, mag_noise_std, 3)
+        for step in range(num_estimation_steps):
+            simulation_step = step * estimation_step_interval
+            acceleration = accelerations[simulation_step]
+            magnetic_field = magnetic_fields[simulation_step]
 
+            # Use TRIAD method for estimation
             estimated_states[step] = triad_method(acceleration, magnetic_field)
 
         estimated_states_all[monte_idx] = estimated_states
@@ -201,18 +254,29 @@ def quaternion_error(true_quat, estimated_quat):
     angle_error = relative_rot.magnitude()
     return np.degrees(angle_error)
 
-def plot_quaternion_error(true_attitudes_all, estimated_attitudes_all, dt=(1 / simulation_hz)):
-    monte_runs, num_steps = true_attitudes_all.shape[0], true_attitudes_all.shape[1]
-    time_array = np.linspace(0, num_steps * dt, num_steps)
+def plot_quaternion_error(true_attitudes_all, estimated_attitudes_all, estimation_step_interval=simulation_hz // estimation_hz, dt=(1 / simulation_hz)):
+    """
+    Plots the quaternion error magnitude over time for each Monte Carlo run.
+
+    Parameters:
+    - true_attitudes_all (np.ndarray): True quaternions (shape: monte_runs x num_steps x 4).
+    - estimated_attitudes_all (np.ndarray): Estimated quaternions (shape: monte_runs x num_estimation_steps x 4).
+    - estimation_step_interval (int): Interval between simulation and estimation steps.
+    - dt (float): Time step between each sample in seconds.
+    """
+    monte_runs, num_estimation_steps = estimated_attitudes_all.shape[0], estimated_attitudes_all.shape[1]
+    time_array = np.linspace(0, num_estimation_steps * dt * estimation_step_interval, num_estimation_steps)
 
     plt.figure(figsize=(12, 6))
     plt.title("Quaternion Error Magnitude Over Time")
 
     for monte_idx in range(monte_runs):
-        quaternion_errors = np.zeros(num_steps)
-        for step in range(num_steps):
-            quaternion_errors[step] = quaternion_error(true_attitudes_all[monte_idx, step],
-                                                       estimated_attitudes_all[monte_idx, step])
+        quaternion_errors = np.zeros(num_estimation_steps)
+        for step in range(num_estimation_steps):
+            true_quat = true_attitudes_all[monte_idx, step * estimation_step_interval]
+            estimated_quat = estimated_attitudes_all[monte_idx, step]
+            quaternion_errors[step] = quaternion_error(true_quat, estimated_quat)
+
         plt.plot(time_array, quaternion_errors)
 
     plt.xlabel('Time (s)')
